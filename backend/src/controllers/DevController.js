cd4e24b693db665a86a59095921d352a56724bae
João Vasconcelos <jvsn19@gmail.com>
2019-12-29 12:51:41 -0300

    Allow create dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,18 @@
+import axios from 'axios'
+import Dev from '../models/Dev'
+
 export default {
-    store(req, res) {
-        return res.json({status: 'OK'})
+    async store({ body: {username} }, res) {
+        const userExists = await Dev.findOne({user: username})
+        
+        if(userExists) {
+            return res.json(userExists)
+        }
+
+        const { data } = await axios.get(`https://api.github.com/users/${username}`)
+        const {name, bio, avatar_url} = data
+        const dev = await Dev.create({name, user: username, bio, avatar: avatar_url})
+
+        return res.json(dev)
     }
 }
