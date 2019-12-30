d1001f68aae3f6a4b2993c84f9c4def0e1d59b70
João Vasconcelos <jvsn19@gmail.com>
2019-12-30 09:04:26 -0300

    Add try catch to avoid bad requests error

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,16 +3,21 @@ import Dev from '../models/Dev'
 
 export default {
     async store({ body: {username} }, res) {
-        const userExists = await Dev.findOne({user: username})
-        
-        if(userExists) {
-            return res.json(userExists)
+        try {
+            const userExists = await Dev.findOne({user: username})
+            
+            if(userExists) {
+                return res.json(userExists)
+            }
+
+            const { data, status } = await axios.get(`https://api.github.com/users/${username}`)
+            const {name, bio, avatar_url} = data
+            const dev = await Dev.create({name, user: username, bio, avatar: avatar_url})
+            
+            return res.json(dev)
         }
-
-        const { data } = await axios.get(`https://api.github.com/users/${username}`)
-        const {name, bio, avatar_url} = data
-        const dev = await Dev.create({name, user: username, bio, avatar: avatar_url})
-
-        return res.json(dev)
+        catch(err) {
+            return res.json(err)
+        }
     }
 }
